1cd5dadcdcfca49ae7d6a1b785b2c80be152d338
Donald <donaldf@vmware.com>
2022-03-08 11:13:10 +0000

    Separating out key variables to use

---
+++ tkg/variables.sh
@@ -0,0 +1,7 @@
+#
+# Main variables that need to be set uniquely for each environment the scripts are run in.
+#
+export VMWUSER=<Username to access the vmware downloads>
+export VMWPASS=<Password>
+
+DOWNLOAD_LOCATION=/path/to/where/you/want/to/download/files
